938156f80dd28ae68a64d55c6a959177b3612e19
fabio <fabio.espinosa@dfki.de>
2024-01-15 14:05:25 +0100

    Fix concatenation bug in pdf_tables_to_json_pipeline.py

--- pdf_tables_to_json_pipeline.py
+++ pdf_tables_to_json_pipeline.py
@@ -47,7 +47,7 @@ with open(f"{output_pdf}/processed_{output_pdf}.html", "w") as file:
         result_div = str(div_element)
 
         # Append the result to variable
-        results = result + result_div
+        results = results + result_div
 
         # Write the result to the file
         file.write(result + "\n")
