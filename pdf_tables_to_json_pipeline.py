e091a27de3ff1e1284e6e478b46920349d5ee554
fabio <fabio.espinosa@dfki.de>
2024-01-16 15:32:50 +0100

    Refactor PDF to JSON pipeline code

--- pdf_tables_to_json_pipeline.py
+++ pdf_tables_to_json_pipeline.py
@@ -1,12 +1,13 @@
 # Script that transforms pdfs to hmtl code and then cleans this code until only tables are left and then using gpt4 transforms this tables into json objects
 
 import subprocess
-import os
-from unittest import result
+import time
 from bs4 import BeautifulSoup, NavigableString
 from openai import OpenAI
 import json
+import pandas as pd
 import tiktoken
+import os
 
 
 def num_tokens_from_string(string: str, model_name: str) -> int:
@@ -34,8 +35,8 @@ def process_html(output_html):
     # Find all div elements with a "data-page-no" attribute
     div_elements = soup.find_all("div", attrs={"data-page-no": True})
 
-    results = ""
-
+    complete_results = ""
+    results_as_array = []
     # Open the output file in write mode
     with open(f"{output_html}/processed_{output_html}.html", "w") as file:
         # Loop over the div elements
@@ -57,13 +58,13 @@ def process_html(output_html):
 
             # Convert the div element back to string
             result_div = str(div_element)
-
+            results_as_array.append(result_div)
             # Append the result to variable
-            results = results + result_div
+            complete_results = complete_results + result_div
 
             # Write the result to the file
-        file.write(results + "\n")
-        return result_div
+        file.write(complete_results + "\n")
+        return complete_results, results_as_array
 
 
 def run_inference(query: str, llm_model: str):
@@ -97,7 +98,7 @@ def run_inference(query: str, llm_model: str):
     return complete_response
 
 
-def save_json(response: str, output_html: str):
+def save_json(response: str, output_path: str):
     """Verifies if response is valid JSON and saves it to json file"""
 
     # Remove ```json and ``` from response
@@ -107,22 +108,81 @@ def save_json(response: str, output_html: str):
     except ValueError:
         print("Response is not valid JSON")
     else:
-        with open(f"{output_html}/processed_{output_html}.json", "w") as file:
+        with open(output_path, "w") as file:
             file.write(response)
 
 
 def main():
-    input_pdf = "XS630B1.pdf"
-    output_name = "XS630B1"
+    # Create array of PDFs to be processed by reading contents of the folder "testing_pdfs" and getting their paths
 
-    convert_pdf_to_html(input_pdf, output_name)
+    folder = "pdfs_to_test"
+    # # Specify the folder
+    pdfs_to_test = [f"{folder}/Datasheet_BCS0037_270030_en.pdf"]
 
-    processed_html = process_html(output_name)
+    # # Get the current working directory
+    # current_directory = os.getcwd()
+
+    # # Create the full path to the folder
+    # folder_path = os.path.join(current_directory, folder)
+
+    # # Get a list of all files and folders in the specified folder
+    # contents = os.listdir(folder_path)
+
+    # # Print the contents
+    # for item in contents:
+    #     if item.endswith(".pdf"):
+    #         pdfs_to_test.append(item)
+    #         print(item)
+
+    print(pdfs_to_test)
 
     openai_model = "gpt-4-1106-preview"
-    response = run_inference(processed_html, openai_model)
+    # process each pdf
+    for pdf in pdfs_to_test:
+        output_name = pdf.replace(".pdf", "")
+        start_time = time.time()
 
-    save_json(response, output_name)
+        convert_pdf_to_html(pdf, output_name)
+
+        whole_html, array_of_html = process_html(output_name)
+
+        complete_response = ""
+        for html in array_of_html:
+            output_path = (
+                f"{output_name}/{output_name}_page_{array_of_html.index(html)}.json"
+            )
+            print(output_path)
+            individual_response = run_inference(html, openai_model)
+
+            save_json(individual_response, output_path)
+            # complete_response += individual_response
+
+        end_time = time.time()
+        running_time = end_time - start_time
+        print("Running time:", running_time, "seconds")
+
+    # input_pdf = "134132_eng.pdf"
+    # output_name = input_pdf.replace(".pdf", "")
+
+    # start_time = time.time()
+
+    # convert_pdf_to_html(input_pdf, output_name)
+
+    # whole_html, array_of_html = process_html(output_name)
+
+    # openai_model = "gpt-4-1106-preview"
+
+    # # complete_response = ""
+    # # for html in array_of_html:
+    # #     individual_response = run_inference(html, openai_model)
+    # #     complete_response += individual_response
+
+    # complete_response = run_inference(whole_html, openai_model)
+    # # save_json(complete_response, output_name)
+
+    # end_time = time.time()
+    # running_time = end_time - start_time
+    # print("Running time:", running_time, "seconds")
 
 
 if __name__ == "__main__":
